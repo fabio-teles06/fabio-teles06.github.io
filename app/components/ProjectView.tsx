643345469b10a3d2c01eb56b790006a6530a2371
fabio-teles06 <fabio.teles06@outlook.com>
2025-03-01 18:15:46 -0300

    enhance ProjectView component with image display and styling; add global CSS and metadata

--- app/components/ProjectView.tsx
+++ app/components/ProjectView.tsx
@@ -1,18 +1,24 @@
+import Link from "next/link";
 import React from "react";
+import { FaGithub } from "react-icons/fa";
 
 // project interface
 interface Project {
     name: string;
     description: string;
+    image: string;
     url: string;
 }
 
 export default function ProjectView({ project }: { project: Project }) {
     return (
-        <div>
-            <h1>{project.name}</h1>
-            <p>{project.description}</p>
-            <a href={project.url}>Link</a>
+        <div className="flex flex-col bg-white dark:bg-gray-800 w-full md:w-1/3 p-6 rounded-lg shadow-xl transition-transform transform hover:scale-105 hover:shadow-2xl">
+            <h1 className="text-xl font-semibold text-gray-800 dark:text-white mb-2">{project.name}</h1>
+            <p className="text-gray-600 dark:text-gray-300 mb-4">{project.description}</p>
+            <img src={project.image} alt={project.name} className="w-full h-56 object-cover rounded-lg mb-4 shadow-md" />
+            <Link href={project.url} className="text-blue-600 dark:text-blue-400 hover:text-blue-800 dark:hover:text-blue-300 flex items-center gap-2 mt-auto">
+                <FaGithub /> <span className="font-semibold">Código</span>
+            </Link>
         </div>
     );
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,12 @@
 import React from "react";
+import { Metadata } from "next";
+
+import './globals.css';
+
+export const metadata : Metadata = {
+    title: 'FabioTeles',
+    description: 'Desenvolvedor Web FullStack',
+}
 
 export default function RootLayout({ children }: {
     children: React.ReactNode
