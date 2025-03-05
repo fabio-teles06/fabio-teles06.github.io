f96ac53d23f9ad8cd6b170b3ad2e3e2b115c434e
fabio-teles06 <fabio.teles06@outlook.com>
2025-03-05 17:39:32 -0300

    Adicionado o CV

--- app/page.tsx
+++ app/page.tsx
@@ -84,6 +84,13 @@ export default function Page() {
                         </Tooltip>
                     ))}
                 </div>
+
+                {/* beatiful button to download CV */}
+                <Link href="documents/cv.pdf" download>
+                    <h1 className="bg-blue-500 text-white font-bold py-2 px-4 rounded mt-8">
+                        Abrir o CV
+                    </h1>
+                </Link>
             </div>
 
 
